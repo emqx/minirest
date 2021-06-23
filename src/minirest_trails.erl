3cf572a473fe72805f80bfb1c730e9c8e03cf8c7
DDDHuang <904897578@qq.com>
2021-06-23 15:47:52 +0800

    improve: remove provider state

--- src/minirest_trails.erl
+++ src/minirest_trails.erl
@@ -12,14 +12,14 @@ module_trails(Module, RootPath) ->
     try
         OpenApis = erlang:apply(Module, ?API_SPEC, []),
         Fun =
-            fun({Path, Metadata, CBState}, Trails) ->
-                Trails ++ handler_trails(Module, Path, Metadata, CBState, RootPath)
+            fun({Path, Metadata}, Trails) ->
+                Trails ++ handler_trails(Module, Path, Metadata, RootPath)
             end,
         lists:foldl(Fun, [], OpenApis)
     catch E:R:S ->
         io:format("Callback Module ~p, ~p ~p ~p", [Module, E, R, S])
     end.
 
-handler_trails(CBModule, Path, Metadata, CBState, RootPath) ->
-    HandlerState = minirest_handler:init_state(CBModule, Metadata, CBState),
+handler_trails(CBModule, Path, Metadata, RootPath) ->
+    HandlerState = minirest_handler:init_state(CBModule, Metadata),
     [trails:trail(RootPath ++ Path, minirest_handler, HandlerState, Metadata)].
