d60eab9ea4dcddf3a6474b43c4d527c1c99f6b22
DDDHuang <44492639+DDDHuang@users.noreply.github.com>
2021-07-07 20:25:43 +0800

    fix: behavior callback return (#55)

--- include/minirest.hrl
+++ include/minirest.hrl
@@ -8,9 +8,11 @@
 -type api_options() :: [api_option()].
 
 -type api() :: {path(), mete_data(), callback_function()}.
+-type apis() :: [api()].
 
 -type schema_name() :: string().
 -type api_schema() :: {schema_name(), map()}.
+-type api_schemas() :: [api_schema()].
 
 -type http_method() :: get | post | put | head | delete | patch | options | connect | trace.
 
